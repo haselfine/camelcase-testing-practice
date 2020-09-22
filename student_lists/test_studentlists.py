4831b51100ea67dc5ecdb90d420ce9346a5729e6
Ben <benhaseltine@gmail.com>
2020-09-22 14:11:41 -0500

    add unittest from is not_enrolled

--- student_lists/test_studentlists.py
+++ student_lists/test_studentlists.py
@@ -78,6 +78,11 @@ class TestStudentLists(TestCase):
     ## TODO write a test that adds some example students to a test class,
     ## then, call is_enrolled for a student who is not enrolled. 
     # Use assertFalse to verify is_enrolled returns False.
+    def test_is_not_enrolled(self):
+        test_class = ClassList(2)
+        test_class.add_student('Lucca')
+        test_class.add_student('Margot')
+        self.assertFalse(test_class.is_enrolled('Mara'))
 
 
     def test_string_with_students_enrolled(self):
